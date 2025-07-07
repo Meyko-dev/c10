8605eea71f879bda054e43f21c8607f37751fda2
Meyko_Dev <raphaelle.noel@gmail.com>
2025-07-07 11:03:48 +0200

    exo 1

---
+++ exo1.js
@@ -0,0 +1,26 @@
+//Ecris une condition qui affichera la nature du chiffre (positif, négatif…) dans la console.
+//Déclare une variable qui contiendra un nombre et teste ta condition avec les valeurs suivantes que tu donneras à la variable : 6, -8 et 0.
+
+let myNumber1=3 
+if (myNumber1 > 0) {
+    console.log("Le nombre est positif  ")
+} else (myNumber1 < 0); {
+    console.log("Le nombre est négatif  ")}
+
+let myNumber2=6 
+if (myNumber2 > 0) {
+    console.log("Le nombre est positif  ")
+} else (myNumber2 < 0); {
+    console.log("Le nombre est négatif  ")}
+
+let myNumber3=-8 
+if (myNumber3 > 0) {
+    console.log("Le nombre est positif  ")
+} else (myNumber1 < 0); {
+    console.log("Le nombre est négatif  ")}
+
+let myNumber4=0 
+if (myNumber4 > 0) {
+    console.log("Le nombre est positif  ")
+} else (myNumber1 < 0); {
+    console.log("Le nombre est négatif  ")}
